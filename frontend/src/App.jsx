70ba3b3ab8dced13717a65615a39ad126c0ad661
younome71 <mehtadarpans2004@gmail.com>
2023-10-13 22:53:54 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,63 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import React from 'react'
+import Footer from './components/Footer'
+import Navbar from './components/navbar'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Profile from "./pages/Profile";
+import Rent from './pages/Rent'
+const Layout = () => {
+  
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+      {
+        path: "/rent",
+        element: <Rent />,
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+])
+
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default App
