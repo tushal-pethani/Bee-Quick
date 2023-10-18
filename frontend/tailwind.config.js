0ae20adb4387c085e51a370e128859a38fb1da88
younome71 <mehtadarpans2004@gmail.com>
2023-10-19 02:18:37 +0530

    Added color theme and fonts to tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:
+      {
+        primary: "#FFFFFF", /*Background */
+        secondary: "#FFC629",/*Buttons, sections */
+        tertiary: "#fff7de", /*Navbar  */
+        text: "#000000",
+        footer: "#1d252c"
+      },
+      fontFamily:
+      {
+        body: ['QuickSand','serif'],
+        title: ['Cinzel','cursive'],
+        button: ['Nunito Sans','serif'],
+        footer: ['Cabin','cursive']
+      }
+    },
   },
   plugins: [],
 }
