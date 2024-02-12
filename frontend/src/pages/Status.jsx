ea918f0ea3b8ae7dc9db06b58b1cc308f02d96bf
younome71 <mehtadarpans2004@gmail.com>
2024-02-13 01:02:56 +0530

    Fixed the Status page bug

--- frontend/src/pages/Status.jsx
+++ frontend/src/pages/Status.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../context/authContext';
 
 function Status() {
   const [rideStatus, setRideStatus] = useState('Searching for a ride...');
-  const [details, setDetails] = useState(null)
+  const [driverDetails, setDetails] = useState(null)
   const [rideFound, setRideFound] = useState(false)
   const {currentUser} = useContext(AuthContext)
   const {userid} = currentUser
@@ -35,9 +35,7 @@ function Status() {
         {rideFound && (
           <div className="text-black bg-tertiary bg-opacity-90 font-footer opacity-100 transition-opacity duration-300 transform translate-y-0 mt-6">
 
-            <p className="text-lg font-semibold">Driver: {driverDetails.driverName}</p>
-            <p className="text-lg font-semibold">Bike Model: {driverDetails.carModel}</p>
-            <p className="text-lg font-semibold">Plate Number: {driverDetails.plateNumber}</p>
+            <p className="text-lg font-semibold">Plate Number: {driverDetails.bike_id}</p>
             <button className="theme-btn-shadow rounded-xl bg-[#FFC629] px-4 py-2 mt-4 text-sm font-Nunito sans text-[#000000] font-normal mobile:text-xs hover:bg-[#FFA500] hover:text-white hover:border-[#000000]" style={{ fontFamily: 'Nunito Sans' }}>Cancel Ride</button>
 
 
